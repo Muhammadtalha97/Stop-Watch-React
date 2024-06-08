19bbc84fa3dc3fc08e4e9b4374ea357bb6dad4e9
Talha Code Lab <127521717+talhacodelab97@users.noreply.github.com>
2024-06-08 11:15:50 +0500

    Stop watch Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [startTime, setStartTime] = useState();
+  const [now, setNow] = useState();
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalRef = useRef();
+
+  function handleStart() {
+    const currentTime = Date.now();
+    setStartTime(currentTime - elapsedTime); 
+    setNow(currentTime);
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+    if (startTime != null) {
+      setElapsedTime(Date.now() - startTime); // Update elapsed time when stopped
+    }
+  }
+
+  function Reset(){
+    setStartTime(Date.now())
+    setNow(Date.now())
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <img className="stopwatch" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Yc8CepzzFKzGGd1jcd9hd5uA9E-35awEjg&s" alt="" />
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <div className="btn">
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={Reset}>Reset</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
